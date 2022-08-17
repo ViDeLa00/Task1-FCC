41ab4578ce9f096dd7b9af3a1181fca9794fcfb4
ViDeLa00 <77484455+ViDeLa00@users.noreply.github.com>
2022-08-17 12:36:49 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from arithmetic_formatter import arithmetic_formatter
+from unittest import main
+
+print(arithmetic_formatter(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+main(module='test_module', exit=False)
